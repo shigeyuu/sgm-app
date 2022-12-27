b18ecd017df19d383533b266d17deafd794f056b
shigeyuu <shigeyuu05@gmail.com>
2022-12-27 18:44:50 +0900

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from tensorflow.keras.preprocessing import image
 
 import numpy as np
 import cv2
-import tensorflow as tfgit 
+import tensorflow as tf
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
